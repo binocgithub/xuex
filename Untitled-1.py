b69fb7df4ec78d5d41f4e34970dc1ac8cbc39ac1
binocgithub <binoc@qq.com>
2022-01-05 13:51:47 +0800

    学习格式化字符串

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,5 +1,13 @@
 print("2**4=",2**4)
-name=input("请输入的你名字：")
-print("welcome",name)
 print("1024*768=",1024*768)
-print("we")
+age =  18.5
+name="Tom"
+weight=75.5
+stuId=21
+print('今年我的年龄是%d岁'%age)
+print('我的名字是%s'%name)
+print('我的体重是%.2f公斤'%weight)
+print ('我的学号是%03d'%stuId)
+print ('我叫%s,今年%d岁了，明年%d，我的体重是%.1f,学号是%03d'%(name,age,age+1,weight,stuId))
+print ('我叫%s,今年%s岁了，明年%s，我的体重是%5s,学号是%6s'%(name,age,age+1,weight,stuId))
+print (f'我叫{name}，今年{age}岁了，明年{age+1}，我的体重是{weight}，学号是{stuId}')
